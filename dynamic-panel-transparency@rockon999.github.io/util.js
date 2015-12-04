ba724b678751afcde384d5312595ad91c64febe6
rockon999 <rockon999@users.noreply.github.com>
2015-12-04 17:41:46 -0600

    Fix logic regression. Implement proper handling of windows 'touching' the panel.

--- dynamic-panel-transparency@rockon999.github.io/util.js
+++ dynamic-panel-transparency@rockon999.github.io/util.js
@@ -12,13 +12,14 @@ function is_maximized(window) {
     if (window.maximized_vertically)
         return true;
     var frame = window.get_frame_rect();
-    log('start --');
+    /* Debugging */
+    /*log('start --');
     log(frame.y);
     log(imports.ui.main.panel.actor.get_height());
     log(window.maximized_horizontally);
     log(frame.width);
     log(window.get_screen().get_size()[0]);
-    log('--end')
+    log('--end')*/
     if (frame.y <= imports.ui.main.panel.actor.get_height()) {
         return (window.maximized_horizontally || frame.width >= (window.get_screen().get_size()[0] - 5))
     }
