e9a8014dc3b4fdf6050e056f884c31bc255285ac
Evan Welsh <contact@evanwelsh.com>
2017-05-29 11:29:45 -0500

    Work on window detection.

---
+++ dynamic-panel-transparency@rockon999.github.io/intellifade.js
@@ -0,0 +1,27 @@
+let check_timeout = 0;
+let do_check = false;
+
+function check() {
+
+}
+
+function runCheck() {
+    /* TODO: Something? Anything?
+        let frame = window.get_frame_rect();
+
+        let scale_factor = imports.gi.St.ThemeContext.get_for_stage(global.stage).scale_factor;
+
+        let height_buffer = MAXIMIZED_HEIGHT_BUFFER * scale_factor;
+        let width_buffer = MAXIMIZED_WIDTH_BUFFER * scale_factor;
+
+        let window_touching_panel = frame.y <= (imports.ui.main.panel.actor.get_height() + height_buffer);
+        let vertical_override = window_touching_panel && frame.height >= imports.ui.main.panel.actor.get_height() - height_buffer;
+        let horizontal_override = window_touching_panel && frame.x <= width_buffer && frame.width >= imports.ui.main.panel.actor.get_height() - width_buffer;
+
+        if (horizontal_override || vertical_override) {
+            return true;
+        } else {
+            return false;
+        }
+                */
+}
