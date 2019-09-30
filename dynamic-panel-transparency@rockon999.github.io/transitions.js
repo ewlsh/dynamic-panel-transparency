843e155175800875c48f36e07d6ce0ed773460cc
Rodrigo Saboya <saboya@users.noreply.github.com>
2019-09-30 20:50:24 -0300

    fixing get_slow_dow_factor calls
    See: https://gitlab.gnome.org/GNOME/gnome-shell/commit/12b8fb15b10fc74ad5b26d72008b2b89e80f0a70

--- dynamic-panel-transparency@rockon999.github.io/transitions.js
+++ dynamic-panel-transparency@rockon999.github.io/transitions.js
@@ -96,7 +96,7 @@ function fade_in() {
     }
 
     if (!Settings.get_hide_corners()) {
-        let speed = St.get_slow_down_factor() * Settings.get_transition_speed();
+        let speed = St.Settings.slow_down_factor * Settings.get_transition_speed();
 
         let maximized = Settings.get_maximized_opacity();
         let unmaximized = !this.status.is_transparent() ? maximized : Settings.get_unmaximized_opacity();
@@ -154,7 +154,7 @@ function fade_out() {
 
     // TODO: Figure out how to write the panel corners in pure CSS.
     if (!Settings.get_hide_corners()) {
-        let speed = St.get_slow_down_factor() * Settings.get_transition_speed();
+        let speed = St.Settings.slow_down_factor * Settings.get_transition_speed();
 
         let unmaximized = Settings.get_unmaximized_opacity();
         let maximized = this.status.is_transparent() ? unmaximized : Settings.get_maximized_opacity();
@@ -205,7 +205,7 @@ function blank_fade_out() {
 
     // TODO: These corners...
     if (!Settings.get_hide_corners()) {
-        let speed = St.get_slow_down_factor() * Settings.get_transition_speed();
+        let speed = St.Settings.slow_down_factor * Settings.get_transition_speed();
 
         let maximized = Settings.get_maximized_opacity();
 
